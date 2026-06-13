fe773ea2db86e3769bb0afbe4584814aa6e8a816
Sahiba Sheikh <sahibasheikh181@gmail.com>
2026-06-13 13:00:26 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String[] args) {
+        int myNumber = 865;
+        int yourNumber;
+        yourNumber = 12;
+        yourNumber = 113;
+//        System.out.println(yourNumber);
+
+        boolean isVegetarian = false;
+//        System.out.println(isVegetarian);
+        String wishes = "Good Morning";
+//        System.out.println(wishes);
+
+       char myCharacter = 'h';
+        System.out.println(myCharacter);
+    }
+}
