054b4de886bb5f266542a586827102090e85f049
Sahiba Sheikh <sahibasheikh181@gmail.com>
2026-06-17 07:59:53 +0530

    Add files via upload

---
+++ arthOperator.java
@@ -0,0 +1,17 @@
+public class arthOperator {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+//        double x = 2;
+//        double y = 3;
+//
+//        System.out.println(x+y);
+    }
+
+}
