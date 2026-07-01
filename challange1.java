8c6974799e71a32f227391e561377b18c3c66167
Sahiba Sheikh <sahibasheikh181@gmail.com>
2026-07-01 12:37:43 +0530

    Add files via upload

---
+++ challange1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class challange1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+       double x = input.nextDouble();
+       double y = input.nextDouble();
+        System.out.println(x*y);
+
+    }
+}
