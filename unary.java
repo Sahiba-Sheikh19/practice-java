aeda8d70c0587404838a972dd47965c8378d3bc1
Sahiba Sheikh <sahibasheikh181@gmail.com>
2026-07-01 12:36:44 +0530

    Add files via upload

---
+++ unary.java
@@ -0,0 +1,24 @@
+public class unary {
+    static void main() {
+        int a = 5;
+        int b = -a ;
+        System.out.println(b);
+
+
+        int x = 5;
+        x = x+1;
+        System.out.println(x);
+
+        x += 1;
+        System.out.println(x);
+
+//        ++x;
+//        System.out.println(x);
+//        x++;
+//        System.out.println(x);
+
+        System.out.println(x++);
+        System.out.println(x);
+
+    }
+}
